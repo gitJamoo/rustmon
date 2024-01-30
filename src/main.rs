4f68c98ebd50cfcd236894478ea1ae6cd3866a53
James <galavantinggeckoguy@gmail.com>
2024-01-29 17:27:01 -0800

    Added first Block

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
-use std::io;
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
-use std::io::stdin;
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
-use tui::backend::TermionBackend;
-use tui::layout::Constraint;
-use tui::Terminal;
-use tui::widgets::{ Block, Row, Table, TableState};
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-use termion::raw::IntoRawMode;
-use termion::input::MouseTerminal;
-use termion::screen::AlternateScreen;
-use termion::event::Key;
-use termion::input::TermRead;
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
 
-let stdout = io::stdout().into_raw_mode()?; // enters raw mode, gives full control of IO
+    thread::sleep(Duration::from_millis(5000));
 
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
-fn main() {
-    println!("Hello, world!");
+    Ok(())
 }
