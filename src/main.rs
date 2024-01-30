0c217be6ea3d97eb42b810c3851c6aaff53d385b
James <galavantinggeckoguy@gmail.com>
2024-01-29 17:33:01 -0800

    Added first display

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,52 @@
 use std::{io, thread, time::Duration};
 use tui::{
-    backend::CrosstermBackend,
+    backend::CrosstermBackend,  // Import the `CrosstermBackend` type
+    backend::Backend,           // Import the `Backend` trait
+    layout::{Constraint, Direction, Layout},
     widgets::{Widget, Block, Borders},
-    layout::{Layout, Constraint, Direction},
-    Terminal
+    Frame, Terminal
 };
+
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{self, DisableMouseCapture, EnableMouseCapture, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    let block = Block::default()
+        .title("Block")
+        .borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+    let block = Block::default()
+        .title("Block 2")
+        .borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
+}
 
 fn main() -> Result<(), io::Error> {
     // setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
     terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-        f.render_widget(block, size);
+        ui(f);
     })?;
 
     thread::sleep(Duration::from_millis(5000));
